9609bf4c1306edd2439814c9c4ecccdc00df2859
George Perakis <george@socital.com>
2019-05-17 12:57:18 +0300

    created plot_confusion_matrix()

--- detector/evaluation.py
+++ detector/evaluation.py
@@ -72,3 +72,53 @@ def plot_keras_history(history):
     plt.ylabel('Accuracy')
     plt.legend()
     plt.show()
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+
+    :param cm:
+    :param classes:
+    :param normalize:
+    :param cmap:
+    :return:
+    """
+
+    if normalize:
+
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        title = 'Normalized confusion matrix'
+
+    else:
+        title = 'Confusion matrix'
+
+    plt.imshow(cm,
+               interpolation='nearest',
+               cmap=cmap)
+
+    plt.title(title)
+    plt.colorbar()
+
+    tick_marks = np.arange(len(classes))
+
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+
+    thresh = cm.max() / 2.
+
+    for i, j in itertools.product(range(cm.shape[0]),
+                                  range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
