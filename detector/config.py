89c1c6fb4c2d8ae677c5c9a7bf3d10b2b1a1a979
George Perakis <george@socital.com>
2019-05-17 12:53:07 +0300

    created config.py and configuration class to use throughout the project

---
+++ detector/config.py
@@ -0,0 +1,22 @@
+"""
+Configuration Class
+A simple class to manage configuration
+"""
+import os
+
+from detector import DATA_DIR
+
+
+class Config:
+    """
+
+    """
+
+    training_dir = os.path.join(DATA_DIR, 'faces', 'training_keras')
+    testing_dir = os.path.join(DATA_DIR, 'faces', 'testing_keras')
+
+    train_batch_size = 8
+    train_number_epochs = 500
+
+    img_width = 150
+    img_height = 150
