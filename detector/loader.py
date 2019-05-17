71a966f16ed0ec3ce91ae2d437c2b86275302c1c
George Perakis <george@socital.com>
2019-05-17 13:02:49 +0300

    created parse_filenames() method for loading the image filepaths from the data directory

--- detector/loader.py
+++ detector/loader.py
@@ -30,3 +30,24 @@ class ImagesLoader:
         self.sub_dirs = sorted(os.listdir(self.image_folder_dataset))
 
         self.file_ext = file_ext
+
+    def parse_filenames(self):
+        """
+
+        :return:
+        """
+        filenames = list()
+        # for each subdirectory we have a number of .pgm files that we want to load
+        for sub_dir in self.sub_dirs:
+
+            # creating the actual path of the sub-directory
+            path = os.path.join(self.image_folder_dataset,
+                                sub_dir,
+                                self.file_ext)
+
+            # for each filename, obtain the target (from the filename's description)
+            # and the actual time series.
+            for fn in glob.glob(path):
+                filenames.append(fn)
+
+        return filenames
