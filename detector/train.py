2bb1b4b37a32babd35930e9058c3d900447825cc
George Perakis <george@socital.com>
2019-05-17 14:02:06 +0300

    created train.py to run model training .

---
+++ detector/train.py
@@ -0,0 +1,34 @@
+from detector.loader import ImagesLoader
+from detector.features import ImageProcess
+from detector.model import SiameseNetworkModel
+from detector.utils import plot_history
+from detector.config import Config
+
+if __name__ == "__main__":
+    loader = ImagesLoader(file_ext="*.jpg")
+
+    df = loader.prepare_dataset(balance_targets=True)
+
+    ipo = ImageProcess(data=df)
+
+    train_df, val_df = ipo.train_test_split()
+
+    x_left_train, x_right_train, targs_train = ipo.get_augmented_images_arrays(train_data=train_df,
+                                                                               n_new=5)
+
+    x_left_val, x_right_val, targs_val = ipo.get_validation_images_arrays(val_data=val_df)
+
+    snm = SiameseNetworkModel()
+
+    # (Config.resize_width, Config.resize_height, 3)
+    snm.build_model_2(img_dimension=Config.img_width)
+
+    hist = snm.fit_model(x_left=x_left_train,
+                         x_right=x_right_train,
+                         y=targs_train,
+                         x_val_left=x_left_val,
+                         x_val_right=x_right_val,
+                         y_val=targs_val,
+                         e=50)
+
+    plot_history(hist)
