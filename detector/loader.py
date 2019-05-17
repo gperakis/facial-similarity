d263e326f500064965699562eeee0fe43a87493d
George Perakis <george@socital.com>
2019-05-17 13:05:27 +0300

    created get_paths_with_targets() method to create the appropriate dataset for the siamese network to run.

--- detector/loader.py
+++ detector/loader.py
@@ -63,3 +63,37 @@ class ImagesLoader:
         folder2 = paths[1].split('/')[-2]
 
         return int(folder1 != folder2)
+
+    def get_paths_with_targets(self,
+                               fnames: List[str],
+                               include_same_image: bool = True):
+        """
+
+        :param fnames:
+        :param include_same_image:
+        :return:
+        """
+        if include_same_image:
+            combs = set([tuple(sorted(t)) for t in product(fnames, repeat=2)])
+        else:
+            # create all 2 images combinations
+            combs = combinations(fnames, 2)
+
+        pic_1_paths = list()
+        pic_2_paths = list()
+
+        targets = list()
+
+        for paths_tuple in combs:
+            is_dif = self.is_different(paths_tuple)
+
+            pic_1_paths.append(paths_tuple[0])
+            pic_2_paths.append(paths_tuple[1])
+
+            targets.append(is_dif)
+
+        data = pd.DataFrame({'image1': pic_1_paths,
+                             'image2': pic_2_paths,
+                             'target': targets})
+
+        return data
